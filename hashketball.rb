9bce745aff95beccb87930a00730c8558d0d4132
Kyle Tomanelli <kyletomanelli@gmail.com>
2020-04-10 01:57:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,9 +129,7 @@ end
 # Write code here
 
 def num_points_scored(player)
-  if(game_hash[:home][:players][player])
-    return game_hash[:home][:players][player][]
-  elsif(game_hash[:away][:players][player])
+  if(game_hash.select
     
   end
   
